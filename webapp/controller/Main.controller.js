a458f58ac6fc3869fd1fcce4d84c6d38f68dae6f
GustavoMMSantos <gustavo-gu99@hotmail.com>
2025-10-09 20:31:05 -0300

    bind com table

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -7,6 +7,24 @@ sap.ui.define([  "sap/ui/core/mvc/Controller"], (Controller) => {
       onItemPress: function (oEvent) {
   const oCtx = oEvent.getParameter("listItem").getBindingContext();
   sap.m.MessageToast.show("Você tocou: " + oCtx.getProperty("name"));
-}
+},
+
+onSearch: function (oEvent) {
+  const sQuery = oEvent.getParameter("newValue") || "";
+  const oList = oEvent.getSource().getParent().getContent()[3]; // pega a Table logo abaixo do SearchField
+  const oBinding = oList.getBinding("items");
+  const aFilters = sQuery ? [ 
+    new sap.ui.model.Filter("name", 
+        sap.ui.model.FilterOperator.Contains, 
+        sQuery) ] : [];
+  oBinding.filter(aFilters);
+},
+onSortName: function () {
+  const oTable = 
+  this.getView().byId("__xmlview0--table") 
+  || this.getView().getContent()[0].getContent()[3]; // fallback
+  const oBinding = oTable.getBinding("items");
+  oBinding.sort(new sap.ui.model.Sorter("name", false));
+},
     });
 });
